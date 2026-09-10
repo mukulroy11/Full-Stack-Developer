943010ce5e66014e944085a0888fad2655a78961
mukul <mukulkumarlkr9097@gmail.com>
2026-09-10 17:15:25 +0530

    improve code usng lastIndex

--- 2_JavaScript/11_Let and const/11_5_Generate sentence/index.js
+++ 2_JavaScript/11_Let and const/11_5_Generate sentence/index.js
@@ -6,8 +6,13 @@
 
 function generateSentence(desc, arr) {
     let baseString = `The ${arr.length} ${desc} are `
+    const lastIndex = arr.length - 1
     for (let i = 0; i < arr.length; i++) {
-        baseString += arr[i] + ", " 
+        if (i === lastIndex) {
+            baseString += arr[i] 
+        } else {
+            baseString += arr[i] + ", " 
+        }
     }
     
     return baseString
