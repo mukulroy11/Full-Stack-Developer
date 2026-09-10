23ae8dc70ea543447f58be0770ff6fe5fbff6f42
mukul <mukulkumarlkr9097@gmail.com>
2026-09-10 11:03:25 +0530

    Improving code by passing parameter in function

--- 2_JavaScript/9_Local Storage/index.js
+++ 2_JavaScript/9_Local Storage/index.js
@@ -14,39 +14,38 @@ let deleteBtn = document.getElementById("delete-btn")
 // console.log(myLeads)
 // console.log(typeof myLeads)
 
-
 let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    render(myLeads)
 }
 
-inputBtn.addEventListener("click",  () => {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    localStorage.setItem("myLeads",JSON.stringify(myLeads))
-    renderLeads()
-});
-
-deleteBtn.addEventListener("click", () => {
-    localStorage.clear()
-    myLeads = []
-    renderLeads()
-})
-
-function renderLeads() {
+function render(leads) {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
 
         // listItems += "<li><a target='_blank' href='" + myLeads[i] +"'>" + myLeads[i] + "</a>"
         // Using Template above list syntx is long
         listItems += `
             <li>
-                <a target='_blank' href="${myLeads[i]}">
-                    ${myLeads[i]}
+                <a target='_blank' href="${leads[i]}">
+                    ${leads[i]}
                 </a>
             </li>    
         `
     } 
     ulEl.innerHTML = listItems
 }
+
+inputBtn.addEventListener("click",  () => {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads",JSON.stringify(myLeads))
+    render(myLeads)
+});
+
+deleteBtn.addEventListener("click", () => {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
