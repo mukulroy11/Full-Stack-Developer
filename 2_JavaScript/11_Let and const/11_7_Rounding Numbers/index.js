724aa5504a01058806127dca9826150c7baf1abc
mukul <mukulkumarlkr9097@gmail.com>
2026-09-10 22:00:25 +0530

    convert string to number

---
+++ 2_JavaScript/11_Let and const/11_7_Rounding Numbers/index.js
@@ -0,0 +1,7 @@
+// Challenge:
+// Round the price in the button down to two decimal places.
+// Don't know which method to use? Google it!
+
+const totalPrice = 420.69235632455
+const btn = document.getElementById("purchase-btn")
+btn.textContent = `Buy €${ Number(totalPrice.toFixed(2)) }`
