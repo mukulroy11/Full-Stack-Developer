51eb6cd3d8f38d94cec8c15080eda595c820b42f
mukul <mukulkumarlkr9097@gmail.com>
2026-09-10 16:16:34 +0530

    log out items in a array

---
+++ 2_JavaScript/11_Let and const/11_1_log out.js
@@ -0,0 +1,9 @@
+let myCourses = ["Learn CSS Animation", "UI Design"]
+
+function course(array) {
+    for (let i = 0; i < array.length; i++) {
+        console.log(array[i])
+    }
+}
+
+course(myCourses)
