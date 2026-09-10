6084331c6e9c92533150e0cfed87bbfa691793ea
mukul <mukulkumarlkr9097@gmail.com>
2026-09-10 11:42:19 +0530

    Array as parameter

---
+++ 2_JavaScript/10_Function with multiple parameters/1_passing parameter/index.js
@@ -0,0 +1,25 @@
+// const welcomeEl = document.getElementById("welcome-el")
+
+// function greetUser(greet, name) {
+//     // welcomeEl.textContent = greet + ", " + name
+//     // or
+//     welcomeEl.textContent = `${greet}, ${name}`
+// }
+
+// greetUser("Hello!", "Roy")
+
+// function add(num1, num2) {
+//     sum = num1 + num2
+//     return sum
+// }
+
+// console.log( add(3, 4))
+// console.log( add(9, 102))
+
+
+
+function getFirst(arr) {
+    return arr[0]
+}
+
+console.log(getFirst([10, 3, 5]))
