432582800b58d3466ae3bc10a56cd2abc5a038db
mukul <mukulkumarlkr9097@gmail.com>
2026-09-13 19:29:32 +0530

    laerning CLI and creating package.json using npm init

---
+++ 4_Node.js/1. The package.json file/server.js
@@ -0,0 +1,15 @@
+console.log("Hello! Node")
+
+// PS E:\Full Stack Developer\4_Node.js\1. The package.json file> node server.js
+// Hello! Node
+// PS E:\Full Stack Developer\4_Node.js\1. The package.json file> node
+// Welcome to Node.js v24.21.0.
+// Type ".help" for more information.
+// > console.log("Hello!")
+// Hello!
+// undefined
+// > 
+// (To exit, press Ctrl+C again or Ctrl+D or type .exit)
+// > 
+
+console.log("we have a package.json!!!")
