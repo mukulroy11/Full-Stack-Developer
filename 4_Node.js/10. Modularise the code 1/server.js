c1cdc5ea78650c22a0725dec8b28931cb4874fea
mukul <mukulkumarlkr9097@gmail.com>
2026-09-23 11:17:05 +0530

    Modularise code 1

---
+++ 4_Node.js/10. Modularise the code 1/server.js
@@ -0,0 +1,33 @@
+import http from 'node:http'
+import { getDataFromDB } from './database/db.js'
+import { sendJSONResponse } from './utils/sendJSONResponse.js'
+
+const PORT = 8000
+
+const server = http.createServer(async (req, res) => {
+    const destinations = await getDataFromDB()
+
+    if (req.url === '/api' && req.method === 'GET') {
+
+        sendJSONResponse(res, 200, destinations)
+
+    } else if (req.url.startsWith('/api/continent') && req.method === 'GET') {
+
+        const continent = req.url.split('/').pop()
+        const filteredData = destinations.filter( (destinations) => {
+            return destinations.continent.toLowerCase() === continent.toLowerCase()
+        })
+
+        sendJSONResponse(res, 200, filteredData)
+
+    } else {
+
+        sendJSONResponse(res, 404, {
+            error: 'Route Not Found', 
+            message: "The requested route does not exist"
+        })
+
+    }
+})
+
+server.listen(PORT, () => console.log(`Connected on port: ${PORT}`))
