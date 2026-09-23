4fc855b82edb7d188bb2b5c4b599ddb380861740
mukul <mukulkumarlkr9097@gmail.com>
2026-09-23 19:48:35 +0530

    CORS

--- 4_Node.js/13. Filter By Query Parameters/utils/sendJSONResponse.js
+++ 4_Node.js/13. Filter By Query Parameters/utils/sendJSONResponse.js
@@ -1,5 +1,7 @@
 export const  sendJSONResponse = (res, statusCode, payLoad) => {
     res.setHeader('Content-Type', 'application/json')
+    res.setHeader('Access-Control-Allow-Origin', '*')      //CORS
+    res.setHeader('Access-Control-Allow-Methods', '*GET') //CORS
     res.statusCode = statusCode
     res.end(JSON.stringify(payLoad))
 }
