9714a61790000b744347b38ef152942fe0337248
mukul <mukulkumarlkr9097@gmail.com>
2026-09-24 22:34:25 +0530

    Routing and Paths

--- 4_Node.js/14. Fullstack Node App/server.js
+++ 4_Node.js/14. Fullstack Node App/server.js
@@ -2,14 +2,15 @@ import http from "node:http"
 
 const PORT = 8000
 
-const server = http.createServer((req, res) => {
+console.log(import.meta.dirname) // give the path of file
 
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'Get')  
-    res.writeHead(200, {'Content-Type': 'text/html', 'Access-Control-Allow-Methods': 'POST'}) // if we set POST in writeHaed it execute above line is ignore because writeHead most priority
-    // res.setHeader('Access-Control-Allow-Origin', '*')     //ignore becausesetHeader does not execute after writeHead(set one time not allow modification) 
-    // res.setHeader('Access-Control-Allow-Methods', 'Get')  // Both are CORS allow accees to APIs 
-    res.end('<html><h1>The server is working!!!</h1></html>') 
+const __dirname = import.meta.dirname // underscore(__) is use to set global variable
+const filepatn = `${__dirname}/public/index.html`
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    res.end()
 })
 
 server.listen(PORT, () => console.log(`Connected on port: ${PORT}`))
