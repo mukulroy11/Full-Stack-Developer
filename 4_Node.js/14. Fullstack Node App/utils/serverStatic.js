30e6026deeb90455ae7ac1789f4968bd8df61a78
mukul <mukulkumarlkr9097@gmail.com>
2026-09-25 15:44:16 +0530

    Serve the frontend

--- 4_Node.js/14. Fullstack Node App/utils/serverStatic.js
+++ 4_Node.js/14. Fullstack Node App/utils/serverStatic.js
@@ -1,16 +1,26 @@
 import path from "node:path"
 import fs from 'node:fs/promises'
 import { sendResponse } from "./sendResponse.js"
+import { getContentType } from "./getContentTyoe.js"
 
 export const serverStatic =  async (req, res, basedir) => {
 
-    const filePath = path.join(basedir, 'public', 'index.html')
+    const publicDir = path.join(basedir, 'public')
+    const filePath = path.join(publicDir, req.url === '/' ? 'index.html' : req.url)
+
+    const ext = path.extname(filePath)
+    const contentType = getContentType(ext)
     
     try {
         const content = await fs.readFile(filePath)
-        sendResponse(res, 200, 'text/html', content)
+        sendResponse(res, 200, contentType, content)
     } catch (err) {
-        console.log(err)
+        if (err.code === 'ENDENT') {
+            const content = await fs.readFile(path.join(publicDir, '404.html'))
+            sendResponse(res, 404, 'text/html', content)
+        } else {
+            sendResponse(res, 500, 'text/html', `<html><h1>Server Error: ${err.code}</h1></html>`)
+        }
     }
     
 }
