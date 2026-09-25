e2c86b1467bf52f6d1bb74adea2e6e8fd2229220
mukul <mukulkumarlkr9097@gmail.com>
2026-09-25 14:06:07 +0530

    Fs Module using async is easy way to read files

--- 4_Node.js/14. Fullstack Node App/server.js
+++ 4_Node.js/14. Fullstack Node App/server.js
@@ -1,30 +1,19 @@
 import http from "node:http"
 import path from "node:path"
-import fs from 'node:fs'
+import fs from 'node:fs/promises'
 
 const PORT = 8000
 
 const __dirname = import.meta.dirname
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
 
     const pathToResource = path.join(__dirname, 'public', 'index.html')
 
-    const content = fs.readFileSync(pathToResource, 'utf8')
-
+    const content = await fs.readFile(pathToResource, 'utf8')
     res.statusCode = 200
     res.setHeader('Content-Type', 'text/html')
     res.end(content)
 })
 
 server.listen(PORT, () => console.log(`Connected on port: ${PORT}`))
-
-// Read Files: fs.readFile() — Reads data from a file.
-
-// Create/Write Files: fs.writeFile() — Writes data to a file, replacing the file if it already exists or creating a new one.
-
-// Update Files: fs.appendFile() — Appends specified content to the end of a file.
-
-// Delete Files: fs.unlink() — Removes or deletes a file from the file system.
-
-// Rename Files: fs.rename() — Renames an existing file or directory.
