72372610afe712ffd20eae558c9de6f7f6e17dc6
mukul <mukulkumarlkr9097@gmail.com>
2026-09-25 14:32:24 +0530

    Serve index.html

--- 4_Node.js/14. Fullstack Node App/server.js
+++ 4_Node.js/14. Fullstack Node App/server.js
@@ -1,19 +1,14 @@
 import http from "node:http"
-import path from "node:path"
-import fs from 'node:fs/promises'
+import { serverStatic } from "./utils/serverStatic.js"
 
 const PORT = 8000
 
 const __dirname = import.meta.dirname
 
-const server = http.createServer(async (req, res) => {
+const server = http.createServer( async (req, res) => {
 
-    const pathToResource = path.join(__dirname, 'public', 'index.html')
+    await serverStatic(req, res, __dirname)
 
-    const content = await fs.readFile(pathToResource) //utf coruppt the content so do not use
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/html')
-    res.end(content)
 })
 
 server.listen(PORT, () => console.log(`Connected on port: ${PORT}`))
--- 4_Node.js/14. Fullstack Node App/utils/serverStatic.js
+++ 4_Node.js/14. Fullstack Node App/utils/serverStatic.js
@@ -1,11 +1,16 @@
 import path from "node:path"
+import fs from 'node:fs/promises'
+import { sendResponse } from "./sendResponse.js"
 
-export const serverStatic = (basedir) => {
+export const serverStatic =  async (req, res, basedir) => {
+
     const filePath = path.join(basedir, 'public', 'index.html')
-    console.log(filePath)
+    
+    try {
+        const content = await fs.readFile(filePath)
+        sendResponse(res, 200, 'text/html', content)
+    } catch (err) {
+        console.log(err)
+    }
+    
 }
-
-// export function serverStatic(basedir) {
-//     const filePath = path.join(basedir, 'public', 'index.html')
-//     console.log(filePath)
-// }
